bb9bddfb3d8bfedd97e2ebaf4e2cd4abb7032cee
VipinKumar-70 <vk20140074250@gmail.com>
2026-05-22 12:33:36 +0530

    CORS error fix

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
-require('dotenv').config();
-const http = require('http');
-const { Server } = require('socket.io');
+require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
 
-const app = require('./app');
-const connectDB = require('./utils/db');
-const setupSocket = require('./socket/socketHandler');
+const app = require("./app");
+const connectDB = require("./utils/db");
+const setupSocket = require("./socket/socketHandler");
 
 connectDB();
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
 });
 
 setupSocket(io);
@@ -22,5 +23,5 @@ setupSocket(io);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
